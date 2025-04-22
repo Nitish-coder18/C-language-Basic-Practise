c4f5870d4489a02210b53df1b2fb43a3fa7699ef
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-04-22 10:06:04 +0530

    FInal (Area Of Rectangle)

--- AreaOfRectangle.c
+++ AreaOfRectangle.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main()
 {
@@ -6,5 +7,20 @@ int main()
     int area;
     area = l * b;
     printf("The Area Of The Rectangle is: %d", area);
+    return 0;
+}
+*/
+
+// Taking Input By The User..!! 
+
+#include<stdio.h>
+int main() {
+    int l;
+    int b;
+        printf ("Enter the length: ");
+        scanf ("%d", &l);
+        printf ("Enter the Breadth: ");
+        scanf ("%d", &b);
+    printf ("The Area Of Rectangle is: %d", l*b);
     return 0;
 }
